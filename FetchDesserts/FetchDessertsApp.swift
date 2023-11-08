75de96ec191f85f2e1c7b032f5b507f83fdedd95
bcaro1221 <brycecaro1378@gmail.com>
2023-11-08 16:36:18 -0500

    Initial Commit

---
+++ FetchDesserts/FetchDessertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchDessertsApp.swift
+//  FetchDesserts
+//
+//  Created by Bryce Caro on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchDessertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
