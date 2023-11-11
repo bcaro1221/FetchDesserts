1dedcbcfa3f306f3143c51f5abfe399ba7b0283e
bcaro1221 <brycecaro1378@gmail.com>
2023-11-10 20:26:38 -0500

    Resolved preview unable to load error with blue background

--- FetchDesserts/Views/DessertPreviewView.swift
+++ FetchDesserts/Views/DessertPreviewView.swift
@@ -13,7 +13,7 @@ struct DessertPreviewView: View {
     
     var body: some View {
         ZStack(alignment: .bottomLeading) {
-            LazyImage(url: dessertPreview.url) { phase in
+            LazyImage(url: nil) { phase in
                 if let image = phase.image {
                     image
                         .resizable()
@@ -24,7 +24,6 @@ struct DessertPreviewView: View {
                     ProgressView()
                         .padding()
                 } else {
-                    // TODO: Implement more robust error handling
                     Color.blue
                 }
             }
